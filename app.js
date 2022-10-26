7ec07b36c856adff6d5b6415df7b95b7cf346571
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-10-26 12:34:53 +0200

    feat: add create todo controller

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 
+const todosRouter = require('./routes/todos.routes');
+
 const app = express();
+
+//Middleware
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
+app.use(todosRouter);
 
 app.listen(9000)
